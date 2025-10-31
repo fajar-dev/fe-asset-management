75995b7d09d196800fda13d82b8df64aa9bbbeec
fajar-dev <fajarrivaldi2015@gmail.com>
2025-10-31 10:15:29 +0700

    fix: auth layout

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -2,7 +2,7 @@
   <div class="h-screen flex items-center justify-center px-4">
     <UPageCard
       variant="subtle"
-      class="max-w-sm w-full"
+      class="max-w-sm w-full bg-elevated/5"
     >
       <slot />
     </UPageCard>
