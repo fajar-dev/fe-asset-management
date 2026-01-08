9d02e0ca927d469f8b6e92757396c6ae6161758c
fajar-dev <fajarrivaldi2015@gmail.com>
2026-01-08 13:58:29 +0700

    fix: middleware

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,13 +1,20 @@
 export default defineNuxtRouteMiddleware((to) => {
-  const cookie = useCookie('accessToken', { path: '/' })
   const publicRoutes = ['/login']
   const isPublicRoute = publicRoutes.includes(to.path)
 
-  if (!cookie.value && !isPublicRoute) {
-    return navigateTo({ path: '/login', query: { redirect: to.fullPath } })
+  // Jangan akses localStorage di server
+  if (import.meta.server) return
+
+  const token = localStorage.getItem('accessToken')
+
+  if (!token && !isPublicRoute) {
+    return navigateTo({
+      path: '/login',
+      query: { redirect: to.fullPath },
+    })
   }
 
-  if (cookie.value && to.path === '/login') {
+  if (token && to.path === '/login') {
     const redirect = (to.query.redirect as string) || '/'
     return navigateTo(redirect)
   }
